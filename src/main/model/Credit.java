2f6396b5f6d31219dc5e06192d7118eb16c9902b
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-09-29 23:37:20 +0100

    SMC-14 Créer la classe Credit avec ses attributs et générer les getters/setters.

--- src/main/model/Credit.java
+++ src/main/model/Credit.java
@@ -1,4 +1,116 @@
 package main.model;
 
+import main.enums.EnumDecision;
+
+import java.time.LocalDateTime;
+
 public class Credit {
+    private Integer id;
+    private LocalDateTime dateCredit;
+    private Double montantDemande;
+    private Double montantOctroye;
+    private Double tauxInteret;
+    private Integer dureeenMois;
+    private String typeCredit;
+    private EnumDecision decision;
+    private Integer person_id;
+
+    public Credit(Integer id, LocalDateTime dateCredit, Double montantDemande, Double montantOctroye, Double tauxInteret,
+                  Integer dureeenMois, String typeCredit, EnumDecision decision, Integer person_id
+    ) {
+        this.id = id;
+        this.dateCredit = dateCredit;
+        this.montantDemande = montantDemande;
+        this.montantOctroye = montantOctroye;
+        this.tauxInteret = tauxInteret;
+        this.dureeenMois = dureeenMois;
+        this.typeCredit = typeCredit;
+        this.decision = decision;
+        this.person_id = person_id;
+    }
+
+    public Credit(LocalDateTime dateCredit, Double montantDemande, Double montantOctroye, Double tauxInteret,
+                  Integer dureeenMois, String typeCredit, EnumDecision decision, Integer person_id
+    ) {
+        this.dateCredit = dateCredit;
+        this.montantDemande = montantDemande;
+        this.montantOctroye = montantOctroye;
+        this.tauxInteret = tauxInteret;
+        this.dureeenMois = dureeenMois;
+        this.typeCredit = typeCredit;
+        this.decision = decision;
+        this.person_id = person_id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateCredit() {
+        return dateCredit;
+    }
+
+    public void setDateCredit(LocalDateTime dateCredit) {
+        this.dateCredit = dateCredit;
+    }
+
+    public Double getMontantDemande() {
+        return montantDemande;
+    }
+
+    public void setMontantDemande(Double montantDemande) {
+        this.montantDemande = montantDemande;
+    }
+
+    public Double getMontantOctroye() {
+        return montantOctroye;
+    }
+
+    public void setMontantOctroye(Double montantOctroye) {
+        this.montantOctroye = montantOctroye;
+    }
+
+    public Double getTauxInteret() {
+        return tauxInteret;
+    }
+
+    public void setTauxInteret(Double tauxInteret) {
+        this.tauxInteret = tauxInteret;
+    }
+
+    public Integer getDureeenMois() {
+        return dureeenMois;
+    }
+
+    public void setDureeenMois(Integer dureeenMois) {
+        this.dureeenMois = dureeenMois;
+    }
+
+    public String getTypeCredit() {
+        return typeCredit;
+    }
+
+    public void setTypeCredit(String typeCredit) {
+        this.typeCredit = typeCredit;
+    }
+
+    public EnumDecision getDecision() {
+        return decision;
+    }
+
+    public void setDecision(EnumDecision decision) {
+        this.decision = decision;
+    }
+
+    public Integer getPerson_id() {
+        return person_id;
+    }
+
+    public void setPerson_id(Integer person_id) {
+        this.person_id = person_id;
+    }
 }
