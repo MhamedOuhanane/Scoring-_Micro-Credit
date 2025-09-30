744e9525ca97b19706dccc80883271bac1ba1bca
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-09-30 10:22:08 +0100

    SMC-17 Définir l’interface PersonRepository avec ses methodes

--- src/main/repository/interfaces/PersonRepository.java
+++ src/main/repository/interfaces/PersonRepository.java
@@ -1,4 +1,9 @@
 package main.repository.interfaces;
 
+import main.model.Person;
+
 public interface PersonRepository {
+    Person inserPerson(Person person);
+    Person findAccount(Integer id);
+    Boolean deleteAccount(Person person);
 }
