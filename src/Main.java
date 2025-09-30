feb5cf0c6acee13e9417265ab7d597dd9370f6a1
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-09-30 23:52:21 +0100

    SMC-20 Realiser la selection, modification et suppression d'un crédit au niveau de couche repository.

--- src/Main.java
+++ src/Main.java
@@ -30,13 +30,26 @@ public class Main {
         Credit credit = new Credit(LocalDateTime.now(), 15000., 12000., 0.05, 6, "Arich", EnumDecision.ACCORDIMMEDIAT, 3);
 
         try {
-            Credit credit1 = creditRepository.inserCredit(credit).orElseThrow(RuntimeException::new);
-            System.out.println("Insertion valid. id: " + credit1.getId() + " | date: " + credit1.getDateCredit() + " | decision: " + credit1.getDecision().getDescription());
-            Credit credit2 = creditRepository.findCredit(2).get();
+//            Credit credit1 = creditRepository.inserCredit(credit).orElseThrow(RuntimeException::new);
+//            System.out.println("Insertion valid. id: " + credit1.getId() + " | date: " + credit1.getDateCredit() + " | decision: " + credit1.getDecision().getDescription());
+//            Credit credit2 = creditRepository.findCredit(2).get();
+//            System.out.println("InseSelection valid. id: " + credit2.getId() + " | date: " + credit2.getDateCredit() + " | decision: " + credit2.getDecision().getDescription());
+            List<Credit> credits = creditRepository.selectCredits();
+            credits.forEach(credit1 -> System.out.println("InseSelection valid. id: " + credit1.getId() + " | date: " + credit1.getDateCredit() + " | decision: " + credit1.getDecision().getDescription()));
+
+            Map<String , Object> updates = new HashMap<>();
+            updates.put("montantDemande", 17000);
+            updates.put("tauxInteret", 0.1);
+            updates.put("dureeenMois", 7);
+            updates.put("decision", "REFUS_AUTOMATIQUE");
+
+            Credit credit2 = creditRepository.updateCredit(1, updates).get();
             System.out.println("InseSelection valid. id: " + credit2.getId() + " | date: " + credit2.getDateCredit() + " | decision: " + credit2.getDecision().getDescription());
+//            if (creditRepository.deleteCredit(credit2)) System.out.println("Suppression valid");
+//            else System.out.println("Suppression invalid");
 
         } catch (RuntimeException e) {
-            System.out.println("Erreur : " + e.getCause().getMessage());
+            System.out.println("Erreur : " + e.getMessage());
         }
     }
 }
--- src/main/repository/impl/CreditRepositoryImpl.java
+++ src/main/repository/impl/CreditRepositoryImpl.java
@@ -8,10 +8,7 @@ import main.utils.DatabaseException;
 
 import java.sql.*;
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class CreditRepositoryImpl implements CreditRepository {
     private final Connection conn = DatabaseConfig.getInstance().getConnection();
@@ -70,17 +67,67 @@ public class CreditRepositoryImpl implements CreditRepository {
     }
 
     @Override
-    public Optional<Credit> updateCredit(Integer id, Map<String, Object> update) {
-        return Optional.empty();
+    public Optional<Credit> updateCredit(Integer id, Map<String, Object> updates) {
+        StringBuilder updateQuery = new StringBuilder("UPDATE credit SET ");
+        int i = 0;
+        for (String key : updates.keySet()) {
+            updateQuery.append(key).append(" = ?, ");
+        }
+        updateQuery = new StringBuilder(updateQuery.substring(0, updateQuery.length() - 2));
+        updateQuery.append(" WHERE id = ?");
+
+        try (PreparedStatement pstmt = conn.prepareStatement(updateQuery.toString())){
+            i = 1;
+            for (Object value : updates.values()) {
+                pstmt.setObject(i++, value);
+            }
+            pstmt.setInt(i, id);
+
+            int rowsAff = pstmt.executeUpdate();
+            if (rowsAff > 0) {
+                return this.findCredit(id);
+            }
+            return Optional.empty();
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getMessage(), e);
+        }
+
     }
 
     @Override
     public List<Credit> selectCredits() {
-        return Collections.emptyList();
+        String selectQuery = "SELECT * FROM credit";
+        try (PreparedStatement stmt = conn.prepareStatement(selectQuery)){
+            ResultSet resultSet = stmt.executeQuery();
+            List<Credit> credits = new ArrayList<>();
+            while (resultSet.next()) {
+                Integer creditId = resultSet.getInt("id");
+                LocalDateTime dateCredit = resultSet.getTimestamp("dateCredit").toLocalDateTime();
+                Double montantDemande = resultSet.getDouble("montantDemande");
+                Double montantOctroye = resultSet.getDouble("montantOctroye");
+                Double tauxInteret = resultSet.getDouble("tauxInteret");
+                Integer dureeenMois = resultSet.getInt("dureeenMois");
+                String typeCredit = resultSet.getString("typeCredit");
+                EnumDecision decision = EnumDecision.valueOf(resultSet.getString("decision"));
+                Integer person_id = resultSet.getInt("person_id");
+
+                credits.add(new Credit(creditId, dateCredit, montantDemande, montantOctroye, tauxInteret, dureeenMois, typeCredit, decision, person_id));
+            }
+            return credits;
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getMessage(), e);
+        }
     }
 
     @Override
     public Boolean deleteCredit(Credit credit) {
-        return null;
+        String deleteQuery = "DELETE FROM credit WHERE id = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(deleteQuery)){
+            pstmt.setInt(1, credit.getId());
+            int rowsAff = pstmt.executeUpdate();
+            return rowsAff > 0;
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getMessage(), e);
+        }
     }
 }
