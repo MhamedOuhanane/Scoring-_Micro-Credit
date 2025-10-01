34097a3fbbbeb0084565e0921f5f1f0c79ff1fbc
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-10-01 15:01:26 +0100

    SMC-25 Concevoir l’interface de service pour la gestion des professionnels et fournir son implémentation.

--- src/main/service/interfaces/ProfessionnelServce.java
+++ src/main/service/interfaces/ProfessionnelServce.java
@@ -1,4 +1,14 @@
 package main.service.interfaces;
 
+import main.model.Professionnel;
+
+import java.util.List;
+import java.util.Map;
+
 public interface ProfessionnelServce {
+    Professionnel ajouterProfessionnel(Professionnel professionnel);
+    Professionnel findProfessionnel(Integer id);
+    Professionnel updateProfessionnel(Integer id, Map<String, Object> update);
+    List<Professionnel> getAllProfessionnels();
+    Boolean deleteProfessionnel(Integer id);
 }
